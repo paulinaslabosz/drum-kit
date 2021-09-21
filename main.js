4d0b7e0cdcc6afd78347b2a5b24488e39a936fa8
Paulina Słabosz <sejmipool@gmail.com>
2021-09-21 13:38:59 +0200

    feature: key animation

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 
+
 const keys = document.querySelectorAll('.key')
-
-// keys.forEach(key => key.classList.add('played'))
-
-
 
 
 
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
+    key.classList.add('played');
 })
 
 
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('played');
+}
+
+
+
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
